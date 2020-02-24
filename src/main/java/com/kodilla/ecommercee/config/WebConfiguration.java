d3f0899c8e37db10e6d39ac8fcc810d03bc2b237
Pawel Pluta <pluta.p@gmail.com>
2020-02-24 22:27:12 +0100

    Login

--- src/main/java/com/kodilla/ecommercee/config/WebConfiguration.java
+++ src/main/java/com/kodilla/ecommercee/config/WebConfiguration.java
@@ -1,4 +1,4 @@
-package com.kodilla.ecommercee;
+package com.kodilla.ecommercee.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
